c7d86fefa90dbe3f54c5fc024a80f805a4186e01
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-03-26 18:12:32 +0800

    Update StringUtil.js

--- Web/JS/utility/StringUtil.js
+++ Web/JS/utility/StringUtil.js
@@ -1,17 +1,23 @@
-function StrUtil(){
+function StringUtil(){
 
-    /** todo 去除字符串结尾的逗号',' '，'不管是中文逗号还是英文逗号
-     *
-     **/
-    this.trimEndComma(str){
-        if(str == null || str == ""){
-            return str; 
-        }
-        var regExp = new RegExp("[,，]+$");
-        var index = null;
-        if((index = str.search(regExp)) == -1){
+    
+    /** todo 根据指定的分隔符，切换单行和多行
+     * @param str 原始字符串
+     * @param delimiter 分隔符
+     * @param toSingle true 转化成单行， false 转化成多行
+     * */
+    function toggleSingleMultiLine(str, delimiter, toSingle){
+        if(str === void(0) || str === null || str === ''){
             return str;
         }
-        return str.substring(0, index);
+        
+        if (toSingle) {
+            var regexpSingle = new RegExp('\\s*' + delimiter+ '\\s*', "g");
+            return str.replace(regexpSingle, delimiter);
+        }
+        var regexpMulti = new RegExp(delimiter, "g");
+        return str.replace(regexpMulti, delimiter + '\n');
     }
+
+    
 }
