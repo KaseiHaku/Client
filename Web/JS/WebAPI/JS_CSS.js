e7e4a48c3d33f921c7b5e4a901034b161aec6f3d
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-04-16 19:23:39 +0800

    Update JS_CSS.js

--- Web/JS/WebAPI/JS_CSS.js
+++ Web/JS/WebAPI/JS_CSS.js
@@ -1,1 +1,19 @@
-/** todo 主要讲解 JS 操作 CSS */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>JS_CSS</title>
+</head>
+<body>
+<div id="root">
+    <div>
+        <h3>查询 Element CSS 属性</h3>
+        <div id="cssDemo"></div>
+        <script>
+            let cssDemo = document.getElementById('cssDemo');
+            window.getComputedStyle(cssDemo, '::first-letter'); // pseudo-element
+        </script>
+    </div><hr/>
+</div>
+</body>
+</html>
