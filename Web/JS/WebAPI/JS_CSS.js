6a3e30969f92224fd52e5a9dcbfeac70b944ed1d
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-04-16 19:35:02 +0800

    Update JS_CSS.js

--- Web/JS/WebAPI/JS_CSS.js
+++ Web/JS/WebAPI/JS_CSS.js
@@ -11,7 +11,18 @@
         <div id="cssDemo"></div>
         <script>
             let cssDemo = document.getElementById('cssDemo');
-            window.getComputedStyle(cssDemo, '::first-letter'); // pseudo-element
+            console.log(window.getComputedStyle(cssDemo, '::first-letter')); // pseudo-element, 查看当前元素，当前的所有 CSS 属性及值
+
+
+            console.log(cssDemo.style); // 猜的，查看当前元素，声明的所有 CSS 属性及值
+        </script>
+    </div><hr/>
+    <div>
+        <h3>设置 Element CSS 属性</h3>
+        <div id="cssSetDemo"></div>
+        <script>
+            let cssSetDemo = document.getElementById('cssSetDemo');
+            cssSetDemo.style.width = '400px'; // 设置宽度
         </script>
     </div><hr/>
 </div>
