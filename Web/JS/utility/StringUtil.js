089b11bdf81241358181d1db6df42196359c1ea2
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-05-05 16:04:05 +0800

    Update StringUtil.js

--- Web/JS/utility/StringUtil.js
+++ Web/JS/utility/StringUtil.js
@@ -43,5 +43,15 @@ function StringUtil(){
         return res;
     }
 
+    /** todo 生成一个 UUID */
+    function generateUUID(){
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+        });
+        return uuid;
+    };
     
 }
