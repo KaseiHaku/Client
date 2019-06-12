5809d55d2c5a7510cbb2deb59187f88ef203fd96
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-06-12 17:09:01 +0800

    Update ajax.js

--- Web/Frame/JQuery/ajax.js
+++ Web/Frame/JQuery/ajax.js
@@ -11,9 +11,8 @@ jqXHR.done(function(data, textStatus, jqXHR){
     console.log('failure');
 }).always(function(data|jqXHR, textStatus, jqXHR|errorThrown){ 
     console.log('complete');
-}).then(function(data, textStatus, jqXHR){
+}).then(function(data, textStatus, jqXHR){  // 1.8 之后
     console.log('then success');
-}, 
-function(jqXHR, textStatus, errorThrown){
+}, function(jqXHR, textStatus, errorThrown){
     console.log('then failure');
 });
