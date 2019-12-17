ea2e214f69618c05b4eff43904df56228332765f
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-12-17 10:18:49 +0800

    Update Selectors.js

--- Web/CSS/Selectors.js
+++ Web/CSS/Selectors.js
@@ -22,16 +22,15 @@ function cssSelectors(){
     document.querySelectorAll('div + p');               // 选择所有 <div> 元素下一个兄弟 <p> 元素
 
     /* Pseudo Element Selector */
-    document.querySelectorAll('div::after');                //
-    document.querySelectorAll('div::before');               //
-    document.querySelectorAll('div::first-letter');         //
-    document.querySelectorAll('div::first-line');           //
-    document.querySelectorAll('div::selection');            //
-    document.querySelectorAll('div::backdrop');             //
+    document.querySelectorAll('div::after');                // 匹配指定元素实际内容之前的元素。本例相当于则在 <div>AAA</div>， AAA 之后插入一个内容，内容由 css content 属性定义  
+    document.querySelectorAll('div::before');               // <div>AAA</div>， AAA 之前插入一个内容，内容由 css content 属性定义  
+    document.querySelectorAll('div::first-letter');         // 选择 div 中的第一个字符
+    document.querySelectorAll('div::first-line');           // 选择 div 中的第一行
+    document.querySelectorAll('div::selection');            // 匹配当前页面中被  鼠标拖动选中的内容
 
     /* Pseudo Class Selector */
-    document.querySelectorAll('div:matches(selector)');     // 选择所有符合 selector 的 <div> 元素
-    document.querySelectorAll('div:not(selector)');         // 选择所有不符合 selector 的 <div> 元素
+    document.querySelectorAll('div:is(selector)');          // 选择所有符合 selector 的 <dib> 元素
+    document.querySelectorAll('div:not(selector)');         // 选择所有不符合 selector 的 <div> 元素    
     document.querySelectorAll('div:nth-child(4n+1)');       // 选择 div 元素中直接子元素位置符合 4n+1 格式的元素， odd 奇数位；event 偶数位
     document.querySelectorAll('div:nth-of-type(4n+1)');     // 选择位置符合 4n+1 格式的 <div> 元素， odd 奇数位；event 偶数位
 }
