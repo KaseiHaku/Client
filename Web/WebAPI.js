f39dbbd8302c1df18fb15dcd5ad21b3bae0645ed
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-12-17 10:35:36 +0800

    Create WebAPI.js

---
+++ Web/WebAPI.js
@@ -0,0 +1,13 @@
+/** 
+WebAPI 接口继承关系 
+    EventTarget <- Node <- Element
+*/
+
+/** 
+WebAPI 接口解释
+    EventTarget         # 表示 HTML 中所有可以触发事件的目标
+    Node                # 表示 HTML 中所有 元素节点，属性节点，文本节点，注释节点
+    Element             # 表示 HTML 中所有 元素节点
+*/
+
+
