5435df7c59b7fdcfbf30d2e5c6335d4e41a0c449
KSF <1291114093@qq.com>
2020-04-20 01:51:19 +0800

    remove js

--- Web/JS/Commonly/Number.js
+++
@@ -1,4 +0,0 @@
-// function Number(){}
-Number.parseInt(str);
-Number.parseFloat(str);
-Number.isNaN(str); // 判断 param 是否是 NaN
--- Web/JS/utility/CursorUtil.js
+++
@@ -1,8 +0,0 @@
-// todo 设置光标位置，并显示，没做兼容性处理
-function setCursorPosition(domObj, position){
-    domObj.focus();
-    domObj.selectionStart = position;
-    domObj.selectionEnd = position;
-}
-    
-    
