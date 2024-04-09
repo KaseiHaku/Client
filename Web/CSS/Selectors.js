396fd2e173824f185b63e1b2f280f1f8d21b140e
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2024-04-09 17:39:37 +0800

    Update Selectors.js

--- Web/CSS/Selectors.js
+++ Web/CSS/Selectors.js
@@ -14,7 +14,9 @@ function cssSelectors(){
     document.querySelectorAll('[attr$=val]');           // 选择所有 attr 属性值以 val 字符串结尾的元素
     document.querySelectorAll('[attr|=val]');           // 选择所有 attr 属性值以 val 字符串开头的元素
 
-    /* Combination and Multiple Selector */
+    /* Combination and Multiple Selector 
+     * @doc https://developer.mozilla.org/en-US/docs/Web/CSS/Child_combinator
+     * */
     document.querySelectorAll('div.clazz');             // 选择所有 <div> 且 class 有 .clazz 的元素
     document.querySelectorAll('div , p');               // 选择所有 <div> 和 <p> 元素
     document.querySelectorAll('div   p');               // 选择所有 <div> 元素里面的所有 <p> 元素
@@ -22,14 +24,18 @@ function cssSelectors(){
     document.querySelectorAll('div ~ p');               // 选择所有 <div> 元素后面的所有兄弟 <p> 元素
     document.querySelectorAll('div + p');               // 选择所有 <div> 元素下一个兄弟 <p> 元素
 
-    /* Pseudo Element Selector */
+    /* Pseudo Element Selector 
+     * @doc https://developer.mozilla.org/en-US/docs/Web/CSS/::first-line
+     * */
     document.querySelectorAll('div::after');                // 匹配指定元素实际内容之前的元素。本例相当于则在 <div>AAA</div>， AAA 之后插入一个内容，内容由 css content 属性定义  
     document.querySelectorAll('div::before');               // <div>AAA</div>， AAA 之前插入一个内容，内容由 css content 属性定义  
     document.querySelectorAll('div::first-letter');         // 选择 div 中的第一个字符
     document.querySelectorAll('div::first-line');           // 选择 div 中的第一行
     document.querySelectorAll('div::selection');            // 匹配当前页面中被  鼠标拖动选中的内容
 
-    /* Pseudo Class Selector */
+    /* Pseudo Class Selector 
+     * @doc https://developer.mozilla.org/en-US/docs/Web/CSS/:has
+     * */
     document.querySelectorAll(':root');                     // 相当于 document.querySelectorAll('html');
     document.querySelectorAll(':scope > p');                // :scope 代表当前执行 querySelectorAll() 方法的元素
     document.querySelectorAll('div:is(selector)');          // 选择所有符合 selector 的 <dib> 元素
